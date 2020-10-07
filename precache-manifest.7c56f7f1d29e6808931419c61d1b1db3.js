6e89909e1068caec387d06ccec785e1d32e1b3f7
Magdalena Checinski <madzix.p730@gmail.com>
2020-10-07 11:26:00 +0200

    Updates

---
+++ precache-manifest.7c56f7f1d29e6808931419c61d1b1db3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04809a3cdca4cfe72020288092ec6588",
+    "url": "/movies-browser/index.html"
+  },
+  {
+    "revision": "4183165c58371b889302",
+    "url": "/movies-browser/static/js/2.f2338c7a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movies-browser/static/js/2.f2338c7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d45df1ca7b9126de1349",
+    "url": "/movies-browser/static/js/main.805798e4.chunk.js"
+  },
+  {
+    "revision": "b4c463465b3baf8f8203",
+    "url": "/movies-browser/static/js/runtime-main.8e42f4f8.js"
+  }
+]);
