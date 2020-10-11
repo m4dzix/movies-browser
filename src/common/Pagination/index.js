55758f8007d453de1b5d2bf6fa1796605e71980c
Lukasz Krzeminski <krzeminski.lukasz@interia.pl>
2020-10-11 11:43:37 +0200

    changing button text depending on resolution

--- src/common/Pagination/index.js
+++ src/common/Pagination/index.js
@@ -4,20 +4,22 @@ import { Container, Button, TextArea, Text} from "./styled";
 
 const Pagination = () => {
 
+    //tmp Data, also disabled state added to 2 buttons for preview
     const currentPage = 7;
     const maxPages = 100;
+    const isMobile = true;
 
     return(
         <Container>
-            <Button disabled>&#60; <Text>First</Text></Button>
-            <Button disabled>&#60; <Text>Previous</Text></Button>
+            <Button disabled>{isMobile ? <>&#60;&#60;</> : <>&#60; <Text>First</Text></>}</Button>
+            <Button disabled>{isMobile ? <>&#60;</> : <>&#60; <Text>Previous</Text></>}</Button>
             <TextArea>
                 <>
                     <Text inTextArea>Page</Text> {currentPage} <Text inTextArea>of</Text> {maxPages} 
                 </>
             </TextArea>
-            <Button><Text>Next</Text> &#62;</Button>
-            <Button><Text>Last</Text> &#62;</Button>
+            <Button>{isMobile ? <>&#62;</> : <>&#62; <Text>Previous</Text></>}</Button>
+            <Button>{isMobile ? <>&#62;&#62;</> : <>&#62; <Text>First</Text></>}</Button>
         </Container>
     );
 };
