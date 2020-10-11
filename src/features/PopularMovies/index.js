73caca027c97ac6fbe30562f736074f605ae0de7
Magdalena Checinski <madzix.p730@gmail.com>
2020-10-11 17:59:20 +0200

    Added a tile as body section

--- src/features/PopularMovies/index.js
+++ src/features/PopularMovies/index.js
@@ -1,11 +1,26 @@
 import React from "react";
 import Main from "../../common/Main";
 import Section from "../../common/Section";
+import Tail from "../../common/Tail";
 
 const PopularMovies = () => {
   return (
     <Main>
-      <Section></Section>
+      <Section
+        title={"Popular movies"}
+        body={
+          <Tail
+            title={"Tytuł"}
+            year={"2020"}
+            type={"action"}
+            imagePath={
+              "https://cdn.pixabay.com/photo/2016/03/04/18/07/bear-1236446_960_720.jpg"
+            }
+            voteAverage={"7,6"}
+            voteCount={"35"}
+          ></Tail>
+        }
+      ></Section>
     </Main>
   );
 };
