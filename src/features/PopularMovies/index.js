056f9723df36f4eaeddefa81e98edcc24a19b0ad
Magdalena Checinski <madzix.p730@gmail.com>
2020-10-11 17:57:00 +0200

    Popular Movies folder created and added to the App.js folder

---
+++ src/features/PopularMovies/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Main from "../../common/Main";
+import Section from "../../common/Section";
+
+const PopularMovies = () => {
+  return (
+    <Main>
+      <Section></Section>
+    </Main>
+  );
+};
+export default PopularMovies;
