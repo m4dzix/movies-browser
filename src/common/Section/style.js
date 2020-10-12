ea5797a002b06c7a3b08108c8f6471c631e3af2d
Magdalena Checinski <madzix.p730@gmail.com>
2020-10-12 11:58:34 +0200

    Media query added to section

--- src/common/Section/style.js
+++ src/common/Section/style.js
@@ -2,8 +2,11 @@ import styled from "styled-components";
 
 export const Container = styled.section`
   display: grid;
-  grid-template-columns: repeat(4, 324px);
+  grid-template-columns: repeat(4, minmax(324px, 1fr));
   grid-gap: 24px;
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    grid-template-columns: repeat(auto-fill, 324px);
+  }
 `;
 
 export const Header = styled.h2`
