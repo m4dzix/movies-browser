21eae4c4c7f733188c59d179227529d682c30326
Magdalena Checinski <madzix.p730@gmail.com>
2020-10-15 20:40:30 +0200

    Created slice to render popular movies

---
+++ src/features/PopularMovies/moviesSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const movieSlice = createSlice({
+  name: "movies",
+  initialState: {
+    loading: true,
+    popularMovies: [],
+  },
+  reducers: {
+    fetchPopularMovies: (state) => {
+      state.loading = true;
+    },
+    fetchPopularMoviesSuccess: (state, { payload: popularMovies }) => {
+      state.loading = false;
+      state.popularMovies = popularMovies.results;
+    },
+    fetchPopularMoviesError: (state) => {
+      state.loading = false;
+      state.popularMovies = !state.popularMovies;
+    },
+  },
+});
+export const {
+  fetchPopularMovies,
+  fetchPopularMoviesSuccess,
+  fetchPopularMoviesError,
+} = moviesSlice.actions;
+export const selectMoviesState = (state) => state.popularMovies;
+export const selectMovies = (state) => selectMoviesState(state).popularMovies;
+export const selectLoading = (state) => selectMoviesState(state).loading;
+
+export default moviesSlice.reducer;
