8101aaf3e94675a259f3c51f467a32fec639c63d
Magdalena Checinski <madzix.p730@gmail.com>
2020-10-16 10:39:56 +0200

    Rendering list of popular movies

--- src/features/PopularMovies/index.js
+++ src/features/PopularMovies/index.js
@@ -1,27 +1,51 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Main from "../../common/Main";
 import Section from "../../common/Section";
 import Tile from "../../common/Tile";
-
+import video from "./Video.svg";
+import {
+  fetchPopularMovies,
+  selectLoading,
+  selectPopularMovies,
+} from "./moviesSlice";
+import { useSelector, useDispatch } from "react-redux";
 const PopularMovies = () => {
-  return (
-    <Main>
-      <Section
-        title={"Popular movies"}
-        body={
-          <Tile
-            title={"Tytuł"}
-            year={"2020"}
-            type={"action"}
-            imagePath={
-              "https://cdn.pixabay.com/photo/2016/03/04/18/07/bear-1236446_960_720.jpg"
-            }
-            voteAverage={"7,6"}
-            voteCount={"35"}
-          ></Tile>
-        }
-      ></Section>
-    </Main>
-  );
+  const loading = useSelector(selectLoading);
+  console.log(loading);
+  const popularMovies = useSelector(selectPopularMovies);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchPopularMovies());
+  }, [dispatch]);
+
+  if (!loading && popularMovies) {
+    return (
+      <Main>
+        <Section
+          title={"Popular movies"}
+          body={popularMovies.map((movie) => (
+            <Tile
+              key={movie.id}
+              title={movie.title}
+              year={movie.release_date.split("-")[0]}
+              type={movie.genre_ids}
+              imagePath={
+                !!movie.poster_path
+                  ? `https://image.tmdb.org/t/p/w185/${movie.poster_path}`
+                  : video
+              }
+              voteAverage={movie.vote_average}
+              voteCount={movie.vote_count}
+            ></Tile>
+          ))}
+        ></Section>
+      </Main>
+    );
+  } else if (loading) {
+    return <div>LOADING LOADING LOADING</div>;
+  } else {
+    return <div>ERROR ERROR </div>;
+  }
 };
 export default PopularMovies;
