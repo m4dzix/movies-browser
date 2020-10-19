21a5912497eac32159fd09d3681cb45ac0d90b69
Magdalena Checinski <madzix.p730@gmail.com>
2020-10-20 01:08:30 +0200

    Rendering list of polular people

---
+++ src/features/People/PopularPeople/index.js
@@ -0,0 +1,48 @@
+import React, { useEffect } from "react";
+import Main from "../../../common/Main";
+import Section from "../../../common/Section";
+import Tile from "../../../common/Tile";
+import { useSelector, useDispatch } from "react-redux";
+
+import {
+  selectLoading,
+  fetchPopularPeople,
+  selectPopularPeople,
+} from "../peopleSlice";
+const PopularPeople = () => {
+  const loading = useSelector(selectLoading);
+  const popularPeople = useSelector(selectPopularPeople);
+  console.log(popularPeople);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchPopularPeople());
+  }, [dispatch]);
+
+  if (!loading && popularPeople) {
+    return (
+      <Main>
+        <Section
+          title={"Popular people"}
+          body={popularPeople.map((people) => (
+            <Tile
+              key={people.id}
+              title={people.name}
+              imagePath={`https://image.tmdb.org/t/p/w185/${people.profile_path}`}
+            ></Tile>
+          ))}
+        ></Section>
+      </Main>
+    );
+  } else if (loading) {
+    return (
+      <div>
+        <h1>LOADING</h1>
+        <p>please wait for a while...</p>;
+      </div>
+    );
+  } else {
+    return <div>ERROR ERROR ERROR</div>;
+  }
+};
+export default PopularPeople;
