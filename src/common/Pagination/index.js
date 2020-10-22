dcea7a554e5d055cd7d82afb312ae83160c5544c
Magdalena Checinski <madzix.p730@gmail.com>
2020-10-22 15:30:35 +0200

    Switching pages after clicking the button

--- src/common/Pagination/index.js
+++ src/common/Pagination/index.js
@@ -1,27 +1,82 @@
 import React from "react";
-import { Container, Button, Pages, Text} from "./styled";
-
-
+import { useDispatch, useSelector } from "react-redux";
+import { Container, Button, Pages, Text } from "./styled";
+import {
+  selectPage,
+  selectTotalPages,
+  nextPage,
+  previousPage,
+  lastPage,
+  firstPage,
+} from "../../features/PopularMovies/moviesSlice";
 const Pagination = () => {
-
-    //tmp Data, also disabled state added to 2 buttons for preview
-    const currentPage = 7;
-    const maxPages = 100;
-    const isMobile = true;
-
-    return(
-        <Container>
-            <Button disabled>{isMobile ? <>&#60;&#60;</> : <>&#60; <Text>First</Text></>}</Button>
-            <Button disabled>{isMobile ? <>&#60;</> : <>&#60; <Text>Previous</Text></>}</Button>
-            <Pages>
-                <>
-                    <Text inPages>Page</Text> {currentPage} <Text inPages>of</Text> {maxPages} 
-                </>
-            </Pages>
-            <Button>{isMobile ? <>&#62;</> : <><Text>Next</Text> &#62;</>}</Button>
-            <Button>{isMobile ? <>&#62;&#62;</> : <><Text>First</Text> &#62;</>}</Button>
-        </Container>
-    );
+  //tmp Data, also disabled state added to 2 buttons for preview
+  const currentPage = useSelector(selectPage);
+  const maxPages = useSelector(selectTotalPages);
+  const isMobile = true;
+  const dispatch = useDispatch();
+  return (
+    <Container>
+      <Button
+        onClick={() => {
+          dispatch(firstPage());
+        }}
+      >
+        {isMobile ? (
+          <>&#60;&#60;</>
+        ) : (
+          <>
+            &#60; <Text>First</Text>
+          </>
+        )}
+      </Button>
+      <Button
+        onClick={() => {
+          dispatch(previousPage());
+        }}
+      >
+        {isMobile ? (
+          <>&#60;</>
+        ) : (
+          <>
+            &#60; <Text>Previous</Text>
+          </>
+        )}
+      </Button>
+      <Pages>
+        <>
+          <Text inPages>Page</Text> {currentPage} <Text inPages>of</Text>{" "}
+          {maxPages}
+        </>
+      </Pages>
+      <Button
+        onClick={() => {
+          dispatch(nextPage());
+        }}
+      >
+        {isMobile ? (
+          <>&#62;</>
+        ) : (
+          <>
+            <Text>Next</Text> &#62;
+          </>
+        )}
+      </Button>
+      <Button
+        onClick={() => {
+          dispatch(lastPage());
+        }}
+      >
+        {isMobile ? (
+          <>&#62;&#62;</>
+        ) : (
+          <>
+            <Text>First</Text> &#62;
+          </>
+        )}
+      </Button>
+    </Container>
+  );
 };
 
 export default Pagination;
