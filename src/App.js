15c317b0538f6755646bddb34c12baf60386ae2f
Magdalena Checinski <madzix.p730@gmail.com>
2020-10-26 17:36:30 +0100

    Created Link to subpage movie details

--- src/App.js
+++ src/App.js
@@ -17,43 +17,52 @@ import {
 import search from "./images/search.svg";
 import camera from "./images/camera.svg";
 import PopularPeople from "./features/People/PopularPeople";
+import MovieDetails from "./features/MovieDetails";
 
-export default () => (
-  <HashRouter>
-    <nav>
-      <NavContainer>
-        <NavContent>
-          <LogoContainer>
-            <StyledImg src={camera} alt="camera icon" />
-            <Title>Movies Browser</Title>
-          </LogoContainer>
-          <LinkContainer>
-            <ListItem>
-              <StyledNavLink to="/popular-movies">movies</StyledNavLink>
-            </ListItem>
-            <ListItem>
-              <StyledNavLink to="/popular-people">people</StyledNavLink>
-            </ListItem>
-          </LinkContainer>
-        </NavContent>
-        <InputContainer>
-          <StyledImg src={search} alt="lens icon" />
-          <Input placeholder="Search for movies..." />
-        </InputContainer>
-      </NavContainer>
-    </nav>
-    <Switch>
-      <Route path="/popular-movies">
-        <PopularMovies />
-        <Pagination />
-      </Route>
-      <Route path="/popular-people">
-        <PopularPeople />
-        <Pagination />
-      </Route>
-      <Route path="/">
-        <Redirect to="/popular-movies" />
-      </Route>
-    </Switch>
-  </HashRouter>
-);
+const App = () => {
+  const toMovie = ({ id } = { id: ":id" }) => `/movie-details/${id}`;
+
+  return (
+    <HashRouter>
+      <nav>
+        <NavContainer>
+          <NavContent>
+            <LogoContainer>
+              <StyledImg src={camera} alt="camera icon" />
+              <Title>Movies Browser</Title>
+            </LogoContainer>
+            <LinkContainer>
+              <ListItem>
+                <StyledNavLink to="/popular-movies">movies</StyledNavLink>
+              </ListItem>
+              <ListItem>
+                <StyledNavLink to="/popular-people">people</StyledNavLink>
+              </ListItem>
+            </LinkContainer>
+          </NavContent>
+          <InputContainer>
+            <StyledImg src={search} alt="lens icon" />
+            <Input placeholder="Search for movies..." />
+          </InputContainer>
+        </NavContainer>
+      </nav>
+      <Switch>
+        <Route path="/popular-movies">
+          <PopularMovies />
+          <Pagination />
+        </Route>
+        <Route path="/popular-people">
+          <PopularPeople />
+          <Pagination />
+        </Route>
+        <Route path={toMovie()}>
+          <MovieDetails />
+        </Route>
+        <Route path="/">
+          <Redirect to="/popular-movies" />
+        </Route>
+      </Switch>
+    </HashRouter>
+  );
+};
+export default App;
--- src/features/PopularMovies/index.js
+++ src/features/PopularMovies/index.js
@@ -4,12 +4,14 @@ import Section from "../../common/Section";
 import Tile from "../../common/Tile";
 import video from "../../assets/Video.svg";
 import { Tag } from "./styled";
+import { Link } from "react-router-dom";
 import {
   fetchPopularMovies,
   fetchGenres,
   selectLoading,
   selectPopularMovies,
   selectGenres,
+  showId,
 } from "./moviesSlice";
 import { useSelector, useDispatch } from "react-redux";
 import starIcon from "../../assets/Vector.svg";
@@ -17,6 +19,7 @@ const PopularMovies = () => {
   const loading = useSelector(selectLoading);
   const popularMovies = useSelector(selectPopularMovies);
   const movieGenres = useSelector(selectGenres);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -31,29 +34,34 @@ const PopularMovies = () => {
     movieGenres
       .filter((item) => item.id === genreId)
       .map((genres) => genres.name);
-
+  const toMovie = ({ id } = { id: "id" }) => `/movie-details/${id}`;
   if (!loading && popularMovies) {
     return (
       <Main>
         <Section
           title={"Popular movies"}
           body={popularMovies.map((movie) => (
-            <Tile
-              key={movie.id}
-              title={movie.title}
-              year={movie.release_date.split("-")[0]}
-              type={movie.genre_ids.map((id) => (
-                <Tag key={id}>{type(id)}</Tag>
-              ))}
-              imagePath={
-                !!movie.poster_path
-                  ? `https://images.tmdb.org/t/p/w185/${movie.poster_path}`
-                  : video
-              }
-              starIcon={starIcon}
-              voteAverage={movie.vote_average}
-              voteCount={`${movie.vote_count} votes`}
-            ></Tile>
+            <Link to={toMovie({ id: movie.id })} key={movie.id}>
+              <Tile
+                onClick={() => {
+                  dispatch(showId());
+                }}
+                key={movie.id}
+                title={movie.title}
+                year={movie.release_date.split("-")[0]}
+                type={movie.genre_ids.map((id) => (
+                  <Tag key={id}>{type(id)}</Tag>
+                ))}
+                imagePath={
+                  !!movie.poster_path
+                    ? `https://images.tmdb.org/t/p/w185/${movie.poster_path}`
+                    : video
+                }
+                starIcon={starIcon}
+                voteAverage={movie.vote_average}
+                voteCount={`${movie.vote_count} votes`}
+              ></Tile>
+            </Link>
           ))}
         ></Section>
       </Main>
