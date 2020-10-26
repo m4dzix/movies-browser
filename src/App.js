aadbda6bae736969b3c77449d1962c7b0f631d47
Magdalena Checinski <madzix.p730@gmail.com>
2020-10-26 21:56:04 +0100

    Added link to subpage with person details

--- src/App.js
+++ src/App.js
@@ -17,43 +17,53 @@ import {
 import search from "./images/search.svg";
 import camera from "./images/camera.svg";
 import PopularPeople from "./features/People/PopularPeople";
+import PersonDetails from "./features/People/PersonDetails";
 
-export default () => (
-  <HashRouter>
-    <nav>
-      <NavContainer>
-        <NavContent>
-          <LogoContainer>
-            <StyledImg src={camera} alt="camera icon" />
-            <Title>Movies Browser</Title>
-          </LogoContainer>
-          <LinkContainer>
-            <ListItem>
-              <StyledNavLink to="/popular-movies">movies</StyledNavLink>
-            </ListItem>
-            <ListItem>
-              <StyledNavLink to="/popular-people">people</StyledNavLink>
-            </ListItem>
-          </LinkContainer>
-        </NavContent>
-        <InputContainer>
-          <StyledImg src={search} alt="lens icon" />
-          <Input placeholder="Search for movies..." />
-        </InputContainer>
-      </NavContainer>
-    </nav>
-    <Switch>
-      <Route path="/popular-movies">
-        <PopularMovies />
-        <Pagination />
-      </Route>
-      <Route path="/popular-people">
-        <PopularPeople />
-        <Pagination />
-      </Route>
-      <Route path="/">
-        <Redirect to="/popular-movies" />
-      </Route>
-    </Switch>
-  </HashRouter>
-);
+export const App = () => {
+  const toPerson = ({ id } = { id: ":id" }) => `/people-details/${id}`;
+
+  return (
+    <HashRouter>
+      <nav>
+        <NavContainer>
+          <NavContent>
+            <LogoContainer>
+              <StyledImg src={camera} alt="camera icon" />
+              <Title>Movies Browser</Title>
+            </LogoContainer>
+            <LinkContainer>
+              <ListItem>
+                <StyledNavLink to="/popular-movies">movies</StyledNavLink>
+              </ListItem>
+              <ListItem>
+                <StyledNavLink to="/popular-people">people</StyledNavLink>
+              </ListItem>
+            </LinkContainer>
+          </NavContent>
+          <InputContainer>
+            <StyledImg src={search} alt="lens icon" />
+            <Input placeholder="Search for movies..." />
+          </InputContainer>
+        </NavContainer>
+      </nav>
+      <Switch>
+        <Route path="/popular-movies">
+          <PopularMovies />
+          <Pagination />
+        </Route>
+        <Route path="/popular-people">
+          <PopularPeople />
+          <Pagination />
+        </Route>
+        <Route path={toPerson()}>
+          <PersonDetails />
+          <Pagination />
+        </Route>
+        <Route path="/">
+          <Redirect to="/popular-movies" />
+        </Route>
+      </Switch>
+    </HashRouter>
+  );
+};
+export default App;
--- src/features/People/PopularPeople/index.js
+++ src/features/People/PopularPeople/index.js
@@ -5,12 +5,13 @@ import Tile from "../../../common/Tile";
 import Loading from "../../../common/Loading";
 import Error from "../../../common/Error";
 import { useSelector, useDispatch } from "react-redux";
-
+import { Link } from "react-router-dom";
 import {
   selectLoading,
   fetchPopularPeople,
   selectPopularPeople,
   selectChangeTileStyle,
+  showId,
 } from "../peopleSlice";
 
 const PopularPeople = () => {
@@ -23,6 +24,8 @@ const PopularPeople = () => {
     dispatch(fetchPopularPeople());
   }, [dispatch]);
 
+  const toPerson = ({ id } = { id: ":id" }) => `/people-details/${id}`;
+
   if (!loading && popularPeople) {
     return (
       <Main>
@@ -30,12 +33,17 @@ const PopularPeople = () => {
           changeTileStyle={changeTileStyle}
           title={"Popular people"}
           body={popularPeople.map((people) => (
-            <Tile
-              changeTileStyle={changeTileStyle}
-              key={people.id}
-              title={people.name}
-              imagePath={`https://image.tmdb.org/t/p/w185/${people.profile_path}`}
-            ></Tile>
+            <Link to={toPerson({ id: people.id })} key={people.id}>
+              <Tile
+                onClick={() => {
+                  dispatch(showId());
+                }}
+                changeTileStyle={changeTileStyle}
+                key={people.id}
+                title={people.name}
+                imagePath={`https://image.tmdb.org/t/p/w185/${people.profile_path}`}
+              ></Tile>
+            </Link>
           ))}
         ></Section>
       </Main>
