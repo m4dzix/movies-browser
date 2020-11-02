9292e9034e11b440872e250a4ffcfe1523e55796
Lukasz Krzeminski <krzeminski.lukasz@interia.pl>
2020-11-02 18:07:12 +0100

    MoviesByQuery joined to PopularMovies

--- src/features/Movies/PopularMovies/index.js
+++ src/features/Movies/PopularMovies/index.js
@@ -4,9 +4,11 @@ import {
   fetchPopularMovies,
   fetchGenres,
   selectLoading,
-  selectPopularMovies,
+  selectMovies,
   selectGenres,
   showId,
+  selectQuery,
+  fetchMoviesByQuery,
 } from "../moviesSlice";
 import video from "../../../assets/Video.svg";
 import starIcon from "../../../assets/Vector.svg";
@@ -16,15 +18,23 @@ import Tile from "../../../common/Tile";
 import { Tag, StyledLink } from "../../../common/Tile/additionalStyled";
 
 const PopularMovies = () => {
+
+  const query = useSelector(selectQuery);
   const loading = useSelector(selectLoading);
-  const popularMovies = useSelector(selectPopularMovies);
   const movieGenres = useSelector(selectGenres);
 
   const dispatch = useDispatch();
 
+  const moviesByQuery = useSelector(selectMovies);
+  const popularMovies = useSelector(selectMovies);
+
+  useEffect(() => {
+    dispatch(fetchMoviesByQuery(query));
+  }, [dispatch, query]);
+
   useEffect(() => {
     dispatch(fetchPopularMovies());
-  }, [dispatch]);
+  }, [dispatch, query]);
 
   useEffect(() => {
     dispatch(fetchGenres());
@@ -37,41 +47,81 @@ const PopularMovies = () => {
 
   const toMovie = ({ id } = { id: "id" }) => `/movie-details/${id}`;
 
-  if (!loading && popularMovies) {
-    return (
-      <Main>
-        <Section
-          title={"Popular movies"}
-          body={popularMovies.map((movie) => (
-            <StyledLink to={toMovie({ id: movie.id })} key={movie.id}>
-              <Tile
-                onClick={() => {
-                  dispatch(showId());
-                }}
-                key={movie.id}
-                title={movie.title}
-                year={movie.release_date.split("-")[0]}
-                type={movie.genre_ids.map((id) => (
-                  <Tag key={id}>{type(id)}</Tag>
-                ))}
-                imagePath={
-                  !!movie.poster_path
-                    ? `https://images.tmdb.org/t/p/w185/${movie.poster_path}`
-                    : video
-                }
-                starIcon={starIcon}
-                voteAverage={movie.vote_average}
-                voteCount={`${movie.vote_count} votes`}
-              ></Tile>
-            </StyledLink>
-          ))}
-        ></Section>
-      </Main>
-    );
-  } else if (loading) {
-    return <div>LOADING LOADING LOADING</div>;
+  if (query === "") {
+    if (!loading && popularMovies) {
+      return (
+        <Main>
+          <Section
+            title={"Popular movies"}
+            body={popularMovies.map((movie) => (
+              <StyledLink to={toMovie({ id: movie.id })} key={movie.id}>
+                <Tile
+                  onClick={() => {
+                    dispatch(showId());
+                  }}
+                  key={movie.id}
+                  title={movie.title}
+                  year={movie.release_date.split("-")[0]}
+                  type={movie.genre_ids.map((id) => (
+                    <Tag key={id}>{type(id)}</Tag>
+                  ))}
+                  imagePath={
+                    !!movie.poster_path
+                      ? `https://images.tmdb.org/t/p/w185/${movie.poster_path}`
+                      : video
+                  }
+                  starIcon={starIcon}
+                  voteAverage={movie.vote_average}
+                  voteCount={`${movie.vote_count} votes`}
+                ></Tile>
+              </StyledLink>
+            ))}
+          ></Section>
+        </Main>
+      );
+    } else if (loading) {
+      return <div>LOADING LOADING LOADING</div>;
+    } else {
+      return <div>ERROR ERROR </div>;
+    }
   } else {
-    return <div>ERROR ERROR </div>;
+    if (!loading && moviesByQuery) {
+      return (
+        <Main>
+          <Section
+            title={`Search results for: ${query}`}
+            body={moviesByQuery.map((movie) => (
+              <StyledLink to={toMovie({ id: movie.id })} key={movie.id}>
+                <Tile
+                  onClick={() => {
+                    dispatch(showId());
+                  }}
+                  key={movie.id}
+                  title={movie.title}
+                  year={movie.release_date.split("-")[0]}
+                  type={movie.genre_ids.map((id) => (
+                    <Tag key={id}>{type(id)}</Tag>
+                  ))}
+                  imagePath={
+                    !!movie.poster_path
+                      ? `https://images.tmdb.org/t/p/w185/${movie.poster_path}`
+                      : video
+                  }
+                  starIcon={starIcon}
+                  voteAverage={movie.vote_average}
+                  voteCount={`${movie.vote_count} votes`}
+                ></Tile>
+              </StyledLink>
+            ))}
+          ></Section>
+        </Main>
+      );
+    }
+    else if (loading) {
+      return <div>LOADING LOADING LOADING</div>;
+    } else {
+      return <div>ERROR ERROR </div>;
+    }
   }
 };
 export default PopularMovies;
