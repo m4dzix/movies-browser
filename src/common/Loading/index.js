6f6bcd96cfd462de96d56b813cdbdef3be70aab5
Lukasz Krzeminski <krzeminski.lukasz@interia.pl>
2020-11-02 18:33:07 +0100

    updated loading text

--- src/common/Loading/index.js
+++ src/common/Loading/index.js
@@ -1,15 +1,20 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectQuery } from "../../features/Movies/moviesSlice";
 import Header from "./../Header";
 import { LoadingWrapper, StyledSpinner } from "./styled";
 
 
-const Loading = () => (
-    <>
-        <Header title="Search results for “INPUT”"/>
-        <LoadingWrapper>
-            <StyledSpinner />
-        </LoadingWrapper>
-    </>
-);
+const Loading = () => {
+    const query = useSelector(selectQuery);
+    return (
+        <>
+            <Header title={`Search results for ${query}`}/>
+            <LoadingWrapper>
+                <StyledSpinner />
+            </LoadingWrapper>
+        </>
+    );
 
+}
 export default Loading;
