237ef24783468a64def382018f2755d6c9e7ac9e
Lukasz Krzeminski <krzeminski.lukasz@interia.pl>
2020-11-02 18:07:56 +0100

    unified selectMovies selector

--- src/features/Movies/moviesSlice.js
+++ src/features/Movies/moviesSlice.js
@@ -81,7 +81,7 @@ export const {
 } = moviesSlice.actions;
 
 export const selectMoviesState = (state) => state.movies;
-export const selectPopularMovies = (state) => selectMoviesState(state).movies;
+export const selectMovies = (state) => selectMoviesState(state).movies;
 export const selectGenres = (state) => selectMoviesState(state).genres;
 export const selectMovieDetails = (state) => selectMoviesState(state).movie;
 export const selectChangeTileStyle = (state) =>
@@ -89,6 +89,5 @@ export const selectChangeTileStyle = (state) =>
 export const selectLoadingMovieDetails = (state) =>
   selectMoviesState(state).loading;
 export const selectLoading = (state) => selectMoviesState(state).loading;
-export const selectMoviesByQuery = (state) => selectMoviesState(state).movies;
 export const selectQuery = (state) => selectMoviesState(state).query;
 export default moviesSlice.reducer;
