6909eb793f5ae6b557fe344fa67abbcdfdde4b7c
Magdalena Checinski <madzix.p730@gmail.com>
2021-08-18 22:01:11 +0200

    Updates

---
+++ precache-manifest.81887fbd5c4085f125c761ef1a094fbb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c74d255b6b1068698258ab4e94583e9f",
+    "url": "/movies-browser/index.html"
+  },
+  {
+    "revision": "9b24b244bb0a3de0dab7",
+    "url": "/movies-browser/static/js/2.551cb53f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movies-browser/static/js/2.551cb53f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acac4e6f8e5bc40b5f8d",
+    "url": "/movies-browser/static/js/main.235af2c7.chunk.js"
+  },
+  {
+    "revision": "b4c463465b3baf8f8203",
+    "url": "/movies-browser/static/js/runtime-main.8e42f4f8.js"
+  },
+  {
+    "revision": "e35e95ed3ddbf721358efcd0f98251b7",
+    "url": "/movies-browser/static/media/Profile.e35e95ed.svg"
+  },
+  {
+    "revision": "5ac444e871cd20728cbd4696d85e5928",
+    "url": "/movies-browser/static/media/Vector.5ac444e8.svg"
+  },
+  {
+    "revision": "27ea078a3b0b52d36e384ff7d7340f2e",
+    "url": "/movies-browser/static/media/Video.27ea078a.svg"
+  },
+  {
+    "revision": "726b97db775ffd60d8016641ac66e3e3",
+    "url": "/movies-browser/static/media/camera.726b97db.svg"
+  },
+  {
+    "revision": "dd14bd7cd95d7a22759a8607ffee844c",
+    "url": "/movies-browser/static/media/danger.dd14bd7c.svg"
+  },
+  {
+    "revision": "d277a94ce34f051b2093089898ea1104",
+    "url": "/movies-browser/static/media/paper-plane.d277a94c.svg"
+  },
+  {
+    "revision": "bdbe6d0745f3db8838815f24ac63317f",
+    "url": "/movies-browser/static/media/search.bdbe6d07.svg"
+  }
+]);
